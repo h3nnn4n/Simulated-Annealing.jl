dd5146a5a249b494bf9058abf7ede7a250152499
h3nnn4n <uber.renan@gmail.com>
2016-06-08 22:46:18 +0000

    Added a logaritmic cooling scheme

--- src/temps.jl
+++ src/temps.jl
@@ -1,3 +1,10 @@
 function linear(t_0, t_n, i, n)
     return t_0 - i * (t_0 - t_n)/n
 end
+
+ln(x) = log(e, x)
+
+function logaritmic(t_0, t_n, i, n)
+    return t_0 - i ^ ( ln(t_0 - t_n) / ln(n) )
+end
+
