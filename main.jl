c319bdfd022a6995da95e601d8343c20169c3d5f
h3nnn4n <uber.renan@gmail.com>
2016-06-08 21:11:11 +0000

    Now draws temp and objective funcion + can turn on/off ploting and progress output

--- main.jl
+++ main.jl
@@ -5,18 +5,21 @@ include("cnf.jl")
 include("temps.jl")
 
 function main(name)
-    size    = readSize(name)
-    formula = readFormula(name)
-    s_i     = map( x -> x == 1, rand(0:1,size))
-    maxIter = 5 * 10^5
-    t_0     = 220.0
-    t_n     = 0.0
-    iter    = 0
-    temp    = linear
-    x       = []
-    y       = []
-    plotInt = 5
-    canDraw = true
+    tic()
+    size     = readSize(name)
+    formula  = readFormula(name)
+    s_i      = map( x -> x == 1, rand(0:1,size))
+    maxIter  = 5 * 10^5
+    t_0      = 100.0
+    t_n      = 0.0
+    iter     = 0
+    temp     = linear
+    x        = []
+    y        = []
+    ytemp    = []
+    plotInt  = 5
+    canDraw  = false
+    progress = false
 
     while iter < maxIter
         T     = temp(t_0, t_n, iter, maxIter)
@@ -29,10 +32,11 @@ function main(name)
 
         if iter % plotInt == 0 || t == 0
             if canDraw
-                push!(x, iter)
-                push!(y, t   )
+                push!(x    , iter)
+                push!(y    , t   )
+                push!(ytemp, T   )
             end
-            println("$iter \t $t")
+            #=println("$iter \t $t")=#
         end
 
         if t == 0
@@ -48,12 +52,20 @@ function main(name)
     end
 
     if canDraw
-        draw(PNG("out.png", 800px, 600px),
+        draw(PNG("out_obj.png", 800px, 600px),
             plot(x = x, y = y, Geom.line,
             Theme(background_color=colorant"white"),
             Guide.xlabel("Time"),
             Guide.ylabel("Objetive function"),
             Guide.title("Simulated Annealing for 3cnf-sat"))
             )
+        draw(PNG("out_temp.png", 800px, 600px),
+            plot(x = x, y = ytemp, Geom.line,
+            Theme(background_color=colorant"white"),
+            Guide.xlabel("Time"),
+            Guide.ylabel("Temperature"),
+            Guide.title("Simulated Annealing for 3cnf-sat"))
+            )
     end
+    return toq()
 end
