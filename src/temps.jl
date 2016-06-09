105e89ed72300a9c6a4ef54115eee018ed615ba8
h3nnn4n <uber.renan@gmail.com>
2016-06-09 09:46:29 -0300

    Implemented cooling scheme using hyperbolic cosine

--- src/temps.jl
+++ src/temps.jl
@@ -8,3 +8,10 @@ function logaritmic(t_0, t_n, i, n)
     return t_0 - i ^ ( ln(t_0 - t_n) / ln(n) )
 end
 
+function hyperbolic_cos(t_0, t_n, i, n)
+     return (t_0 - t_n) / cosh(10.0 * (i/n))
+end
+
+function accept_default(Δc, T, t_0)
+    return exp( -Δc / T )
+end
