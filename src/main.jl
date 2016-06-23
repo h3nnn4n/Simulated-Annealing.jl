69b0c2436fbd687bb3e466028e38cd0c6c524701
h3nnn4n <uber.renan@gmail.com>
2016-06-23 17:51:58 -0300

    Added multilayer smoothered plot + added a few more control flags.

--- src/main.jl
+++ src/main.jl
@@ -6,24 +6,26 @@ include("temps.jl")
 
 function main(name)
     tic()
-    size     = readSize(name)
-    formula  = readFormula(name)
-    s_i      = map( x -> x == 1, rand(0:1,size))
-    maxIter  = 5 * 10^5
-    t_0      = 20.0
-    t_n      = 0.0
-    iter     = 0
-    temp     = hyperbolic_cos
-    acceptf  = accept_default
-    x        = []
-    y        = []
-    ytemp    = []
-    yprob    = []
-    plotInt  = 10^3
-    canDraw  = false
-    progress = false
-    scale    = 50.0
-
+    size           = readSize(name)
+    formula        = readFormula(name)
+    s_i            = map( x -> x == 1, rand(0:1,size))
+    maxIter        = 5 * 10^5
+    t_0            = 50.0
+    t_n            = 0.0
+    iter           = 0
+    temp           = hyperbolic_cos
+    acceptf        = accept_default
+    x              = []
+    y              = []
+    ytemp          = []
+    plotInt        = 1 * 10^4
+    progressInt    = 5 * 10^3
+    canDraw        = false
+    progress       = false
+    scale          = 50.0
+    stopOnSolution = false
+    solved         = false
+    solvedIter     = 0
     while iter < maxIter
         T     = temp(t_0, t_n, iter, maxIter)
         iter += 1
@@ -33,20 +35,27 @@ function main(name)
         p     = rand()
         Δc    = t_new - t
 
-        if iter % plotInt == 0 || t == 0
-            if canDraw
-                push!(x    , iter          )
-                push!(y    , t             )
-                push!(ytemp, T             )
-                push!(yprob, acceptf(Δc, T, t_0))
-            end
-            if progress
-                println("$(100.0*iter/maxIter) \t $(t) \t $T \t $Δc \t $(acceptf(Δc, T, t_0))")
-            end
+        if canDraw && ( iter % plotInt == 0 || t == 0 )
+            push!(x    , iter          )
+            push!(y    , t             )
+            push!(ytemp, T             )
+        end
+
+        if progress && ( iter % progressInt == 0 )
+            println("$(100.0*iter/maxIter) \t $(t) \t $T \t $Δc \t $(acceptf(Δc, T, t_0))")
+        end
+
+        if !solved
+            solvedIter = iter
         end
 
         if t == 0
-            break
+            if !solved
+                solved = true
+            end
+            if stopOnSolution
+                break
+            end
         end
 
         if Δc < 0.0
@@ -57,27 +66,37 @@ function main(name)
     end
 
     if canDraw
+        name  = @sprintf("out_obj_%s.png", splitext(basename(name))[1])
         draw(PNG("out_obj.png", 800px, 600px),
-            plot(x = x, y = y, Geom.line,
-            Theme(background_color=colorant"white"),
-            Guide.xlabel("Time"),
-            Guide.ylabel("Objetive function"),
-            Guide.title("Simulated Annealing for 3cnf-sat"))
+            plot(
+                layer( x = x, y = y,
+                    Geom.point, Geom.line,   Theme(default_color=colorant"orange")
+                ),
+                layer( x = x, y = y,
+                    Geom.point, Geom.smooth, Theme(default_color=colorant"purple")
+                ),
+                Theme(background_color=colorant"white"),
+                Guide.xlabel("Time"),
+                Guide.ylabel("Objective Function"),
+                Guide.title("Simulated Annealing for 3cnf-sat")
             )
-        draw(PNG("out_temp.png", 800px, 600px),
-            plot(x = x, y = ytemp, Geom.line,
-            Theme(background_color=colorant"white"),
-            Guide.xlabel("Time"),
-            Guide.ylabel("Temperature"),
-            Guide.title("Simulated Annealing for 3cnf-sat"))
+        )
+        name  = @sprintf("out_temp_%s.png", splitext(basename(name))[1])
+        draw(PNG(name, 800px, 600px),
+            plot(
+                layer( x = x, y = ytemp,
+                    Geom.point, Geom.line,   Theme(default_color=colorant"orange")
+                ),
+                layer( x = x, y = ytemp,
+                    Geom.point, Geom.smooth, Theme(default_color=colorant"purple")
+                ),
+                Theme(background_color=colorant"white"),
+                Guide.xlabel("Time"),
+                Guide.ylabel("Temperature"),
+                Guide.title("Simulated Annealing for 3cnf-sat")
             )
-        draw(PNG("out_prob.png", 800px, 600px),
-            plot(x = x, y = yprob, Geom.line,
-            Theme(background_color=colorant"white"),
-            Guide.xlabel("Time"),
-            Guide.ylabel("Probability"),
-            Guide.title("Simulated Annealing for 3cnf-sat"))
-            )
+        )
     end
-    return iter < maxIter ? (toq(), iter, true) : (toq(), iter, false)
+
+    return (toq(), solvedIter, solved)
 end
